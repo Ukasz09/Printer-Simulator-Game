4b45f337549c19431fe6ccf928a513771a4eddf5
Ukasz09 <246703@student.pwr.edu.pl>
2020-03-25 23:36:46 +0100

    Upgraded run script by adding flag support and default path fetching

--- runGame.sh
+++ runGame.sh
@@ -1,18 +1,34 @@
-#!/usr/bin/bash 
+#!/usr/bin/bash
+# Copyright (C) 2020 Lukasz Gajerski <https://github.com/Ukasz09>
 
-$1/bin/java --module-path /usr/lib/jvm/$1/lib/javafx.base.jar:\
-$1/lib/javafx.controls.jar:\
-$1/lib/javafx.fxml.jar:\
-$1/lib/javafx.graphics.jar:\
-$1/lib/javafx.media.jar:\
-$1/lib/javafx.swing.jar:\
-$1/lib/javafx.web.jar:\
-$1/lib/javafx-swt.jar:\
-$1/lib/javafx.base.jar:\
-$1/lib/javafx.controls.jar:\
-$1/lib/javafx.fxml.jar:\
-$1/lib/javafx.graphics.jar:\
-$1/lib/javafx.media.jar:\
-$1/lib/javafx.swing.jar:\
-$1/lib/javafx.web.jar:\
-$1/lib/javafx-swt.jar --add-modules ALL-MODULE-PATH -jar PrinterSimulator2.jar
+##### FLAGS MAINTAINING
+for arg in "$@"
+do
+    case $arg in
+        -d|--default)
+		JAVA_VERSION_NUMBER=$(java -version 2>&1 | grep -i version | cut -d'"' -f2 | cut -d'.' -f1-2)
+		JAVA_PATH=/usr/lib/jvm/java-$JAVA_VERSION_NUMBER-oracle        
+	shift;;
+	-p=*|--path=*)
+		JAVA_PATH="${arg#*=}"
+	shift;;
+    esac
+done
+
+##### MAIN
+$JAVA_PATH/bin/java --module-path $JAVA_PATH/lib/javafx.base.jar:\
+$JAVA_PATH/lib/javafx.controls.jar:\
+$JAVA_PATH/lib/javafx.fxml.jar:\
+$JAVA_PATH/lib/javafx.graphics.jar:\
+$JAVA_PATH/lib/javafx.media.jar:\
+$JAVA_PATH/lib/javafx.swing.jar:\
+$JAVA_PATH/lib/javafx.web.jar:\
+$JAVA_PATH/lib/javafx-swt.jar:\
+$JAVA_PATH/lib/javafx.base.jar:\
+$JAVA_PATH/lib/javafx.controls.jar:\
+$JAVA_PATH/lib/javafx.fxml.jar:\
+$JAVA_PATH/lib/javafx.graphics.jar:\
+$JAVA_PATH/lib/javafx.media.jar:\
+$JAVA_PATH/lib/javafx.swing.jar:\
+$JAVA_PATH/lib/javafx.web.jar:\
+$JAVA_PATH/lib/javafx-swt.jar --add-modules ALL-MODULE-PATH -jar *.jar
