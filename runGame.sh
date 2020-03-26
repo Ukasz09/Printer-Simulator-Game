9d775a3651b7ecd9ad249bf21b1d7c24097880bf
Ukasz09 <246703@student.pwr.edu.pl>
2020-03-26 12:11:15 +0100

    Upgraded runGame script

--- runGame.sh
+++ runGame.sh
@@ -1,19 +1,48 @@
 #!/usr/bin/bash
 # Copyright (C) 2020 Lukasz Gajerski <https://github.com/Ukasz09>
 
+##### CONSTANTS
+MIN_JAVA_VERSION=8
+
+##### FUNCTIONS
+check_version_correctness(){
+	if [ "$JAVA_VERSION_NUMBER" -lt $MIN_JAVA_VERSION ]; then
+		echo "Error:  Yout java version is to incorrect. You need at least java $MIN_JAVA_VERSION" 1>&2
+		exit 1
+	fi
+}
+
+find_java_version()
+{
+	JAVA_VERSION_NUMBER=$(java -version 2>&1 | grep -i version | cut -d'"' -f2 | cut -d'.' -f1-2)
+	check_version_correctness
+}
+
+find_java_library_folder()
+{ 
+ find_java_version	
+ if [[ -d /usr/lib/jvm/java-$JAVA_VERSION_NUMBER-oracle ]]; then
+	JAVA_PATH=/usr/lib/jvm/java-$JAVA_VERSION_NUMBER-oracle
+ elif [[ -d /usr/lib/jvm/java-$JAVA_VERSION_NUMBER ]]; then
+	JAVA_PATH=/usr/lib/jvm/java-$JAVA_VERSION_NUMBER
+ else 
+	echo "Error: Not found java installation folder" 1>&2
+	exit 1
+ fi
+}
+
 ##### FLAGS MAINTAINING
-for arg in "$@"
-do
+ for arg in "$@"
+ do
     case $arg in
         -d|--default)
-		JAVA_VERSION_NUMBER=$(java -version 2>&1 | grep -i version | cut -d'"' -f2 | cut -d'.' -f1-2)
-		JAVA_PATH=/usr/lib/jvm/java-$JAVA_VERSION_NUMBER-oracle        
-	shift;;
+		find_java_library_folder
+		shift;;
 	-p=*|--path=*)
 		JAVA_PATH="${arg#*=}"
-	shift;;
+		shift;;
     esac
-done
+ done
 
 ##### MAIN
 $JAVA_PATH/bin/java --module-path $JAVA_PATH/lib/javafx.base.jar:\
